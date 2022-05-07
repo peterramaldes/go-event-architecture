b992516bc80d74e3ee36a72c416111d55259d09a
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-05-07 19:46:33 -0300

    Added example of consumer and produce using kafka

--- bus/main.go
+++ bus/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	topic          = "message-log"
+	broker1Address = "localhost:9093"
+	broker2Address = "localhost:9094"
+	broker3Address = "localhost:9095"
+)
 
 func main() {
-	log.Println("Bus")
+	ctx := context.Background()
+	go produce(ctx)
+	consume(ctx)
+}
+
+func produce(ctx context.Context) {
+	i := 0
+
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{broker1Address, broker2Address, broker2Address},
+		Topic:   topic,
+	})
+
+	for {
+		err := w.WriteMessages(ctx, kafka.Message{
+			Key:   []byte(strconv.Itoa(i)),
+			Value: []byte("this is message" + strconv.Itoa(i)),
+		})
+
+		if err != nil {
+			panic("could not write message " + err.Error())
+		}
+
+		fmt.Println("writes: ", i)
+		i++
+
+		time.Sleep(time.Second)
+	}
+}
+
+func consume(ctx context.Context) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{broker1Address, broker2Address, broker2Address},
+		Topic:   topic,
+		GroupID: "my-group",
+	})
+
+	for {
+		msg, err := r.ReadMessage(ctx)
+		if err != nil {
+			panic("could not read message " + err.Error())
+		}
+
+		fmt.Println("received: ", string(msg.Value))
+	}
 }
